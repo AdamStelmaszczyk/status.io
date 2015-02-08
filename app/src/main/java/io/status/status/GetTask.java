f1467bc6028ec64f2d966a09b13edf7c0ae6abd8
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2015-02-08 02:05:13 +0100

    Merge

--- app/src/main/java/io/status/status/GetTask.java
+++ app/src/main/java/io/status/status/GetTask.java
@@ -1,7 +1,6 @@
 package io.status.status;
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.google.gson.Gson;
 
@@ -11,11 +10,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * @author Adam Stelmaszczyk
  */
 public class GetTask extends AsyncTask<String, Void, String> {
+    MainActivity caller;
+
+    GetTask(MainActivity caller){
+        this.caller = caller;
+    }
 
     protected String doInBackground(String... urls) {
         HttpClient client = new DefaultHttpClient();
@@ -34,8 +39,15 @@ public class GetTask extends AsyncTask<String, Void, String> {
         if (response == null) {
             return;
         }
-        Log.d("test", "GET response: " + response);
+
         Gson gson = new Gson();
         User user = gson.fromJson(response, User.class);
+
+        LinkedList<String> users = new LinkedList<String>();
+        for (User u : user.friends) {
+            users.push(u.name);
+        }
+
+        this.caller.setupList(users);
     }
 }
--- app/src/main/java/io/status/status/MainActivity.java
+++ app/src/main/java/io/status/status/MainActivity.java
@@ -3,20 +3,37 @@ package io.status.status;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.LinkedList;
 
 /**
  * @author Adam Stelmaszczyk
  */
 public class MainActivity extends ActionBarActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        this.listView = (ListView) findViewById(R.id.listView);
+
 
         String nextAlarm = Settings.System.getString(getContentResolver(),
                 Settings.System.NEXT_ALARM_FORMATTED);
-//        Log.d("test", "nextAlarm:" + nextAlarm);
+//       Log.d("test", "nextAlarm:" + nextAlarm);
+
+        new GetTask(this).execute("http://178.62.45.23/get/1");
+
+        Repeater.mainActivity = this;
+    }
+
+    public void setupList(LinkedList<String> users){
+        ArrayAdapter userListAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, users.toArray());
+        listView.setAdapter(userListAdapter);
     }
 
     @Override
